8c868fa4404e5b9e1c90b205da6d8a2709dad39f
Duddino <duddino@duddino.com>
2025-11-21 10:30:00 +0100

    Default to 20GB if there is no content length

--- src-tauri/src/explorer/mod.rs
+++ src-tauri/src/explorer/mod.rs
@@ -142,7 +142,8 @@ async fn download_checkpoint(
     if !request.status().is_success() {
         return Err(PIVXErrors::ServerError);
     }
-    let content_length = request.content_length().unwrap();
+    // Default to 20GB if there is no content length
+    let content_length = request.content_length().unwrap_or(20_000_000_000.0);
     for name in ["blocks", "chainstate", "sporks", "zerocoin"] {
         let p = data_dir.join(name);
         if p.is_dir() {
